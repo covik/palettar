4a274baa3c236fc8d5e655875373106b3d854c95
Mate Nakić <mate.nakic@zth.dev>
2024-10-17 11:55:09 +0000

    fix(core): allow only legal named CSS colors and color formats

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,17 @@
 import * as CSS from 'csstype'
 
+type Color = CSS.DataType.NamedColor
+    | `#${string}`
+    | `rgb(${number},${number},${number})`
+    | `rgba(${number},${number},${number},${number})`
+    | `hsl(${number},${number}%,${number}%)`
+    | `hsla(${number},${number}%,${number}%,${number})`;
+
 export interface PaletteItem {
-    backgroundColor: CSS.Properties['backgroundColor'];
-    borderColor: CSS.Properties['borderColor'];
-    textColor: CSS.Properties['color'];
-    textLargeColor: CSS.Properties['color'];
+    backgroundColor: Color;
+    borderColor: Color;
+    textColor: Color;
+    textLargeColor: Color;
 }
 
 export interface Palette extends Record<string, PaletteItem> {}
